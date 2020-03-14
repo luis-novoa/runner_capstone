e37bbb1c09836927ea7294f2b5fe10ecdd3d2b55
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-14 17:36:36 -0400

    Adds validations and tests for the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:test_user) {User.new}
+
+  describe 'validations' do
+    it 'check for name presence' do
+      expect(test_user.save).to eq(false)
+    end
+
+    it "check for name's minimum characters" do
+      test_user.name = 'a'
+      expect(test_user.save).to eq(false)
+    end
+
+    it "check for name's maximum characters" do
+      test_user.name = 'a'*21
+      expect(test_user.save).to eq(false)
+    end
+  end
 end
