8c2a647f58d5e35d582977f1e1784a8e552a57a8
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-14 23:34:41 -0400

    Create home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
   resources :sessions, only: [:create, :destroy]
