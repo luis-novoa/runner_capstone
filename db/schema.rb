2d679c27133c6496dc5f7698f8f9d0b899f1684c
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-15 15:33:15 -0400

    Creates Group and Transaction models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_210852) do
+ActiveRecord::Schema.define(version: 2020_03_15_193200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.string "icon"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
+  create_table "transactions", force: :cascade do |t|
+    t.string "name"
+    t.integer "amount"
+    t.bigint "user_id", null: false
+    t.bigint "group_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id"], name: "index_transactions_on_group_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +41,7 @@ ActiveRecord::Schema.define(version: 2020_03_14_210852) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "groups", "users"
+  add_foreign_key "transactions", "groups"
+  add_foreign_key "transactions", "users"
 end
