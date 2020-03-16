0837bf18d0bc1e6a94b28f46a0e26fe453557908
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-16 16:30:56 -0400

    Adds 'all my transactions' link to the menu

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -4,6 +4,13 @@ require 'spec_helper'
 RSpec.describe "layouts/application.html.erb", type: :feature do
   let(:test_user) { User.new(name: 'test') }
 
+  def login
+    test_user.save
+    visit root_path
+    fill_in 'Name', with: 'test'
+    click_button 'Log In'
+  end
+
   scenario 'logged user can logout' do
     test_user.save
     visit root_path
@@ -12,5 +19,12 @@ RSpec.describe "layouts/application.html.erb", type: :feature do
     click_link 'Log out'
     visit new_transaction_path
     expect(current_path).to eq(root_path)
+  end
+
+  scenario "there is a link to check all user's transactions" do
+    login
+    visit new_transaction_path
+    click_link 'All my Transactions'
+    expect(current_path).to eq(user_path(test_user))
   end
 end
