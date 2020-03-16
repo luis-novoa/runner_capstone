b066a0d6a73b566fe8e2134bed3cce54a8f0b09b
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-16 13:25:29 -0400

    Add test for the logout link

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 require 'spec_helper'
 
-# RSpec.describe "layouts/application.html.erb", type: :feature do
-#   let(:test_user) { User.new(name: 'test') }
+RSpec.describe "layouts/application.html.erb", type: :feature do
+  let(:test_user) { User.new(name: 'test') }
 
-#   scenario 'logged user have access to the logout link' do
-#     test_user.save
-#     visit root_path
-#     fill_in 'Name', with: 'test'
-#     click_button 'Log In'
-#     expect(page).to have_link(session_path(session[:session_id]))
-#   end
-# end
+  scenario 'logged user can logout' do
+    test_user.save
+    visit root_path
+    fill_in 'Name', with: 'test'
+    click_button 'Log In'
+    click_link 'Log out'
+    visit new_transaction_path
+    expect(current_path).to eq(root_path)
+  end
+end
