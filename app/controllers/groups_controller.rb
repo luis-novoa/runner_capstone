0d07f269ef3f9120f0a6b579e8de70438bfb9c8b
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-17 16:12:29 -0400

    Implement group creation

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,10 +4,22 @@ class GroupsController < ApplicationController
   end
 
   def create
-    
+    @new_group = current_user.groups.build(group_params)
+    if @new_group.save
+      redirect_to groups_path
+    else
+      flash.now[:alert] = @new_group.errors.full_messages
+      @group = @new_group
+      render 'new'
+    end
   end
 
   def index
     @all_groups = Group.all
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
