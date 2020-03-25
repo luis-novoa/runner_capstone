3c3188cca1a90e9709513c4a15824126710c37db
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-25 11:12:12 -0400

    Refactor tests for login page

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -2,43 +2,40 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe "users/new.html.erb", type: :feature do
-  let(:test_user) { User.new(name: 'test') }
+  # let(:test_user) { User.new(name: 'test') }
 
   scenario 'logging in with inexistent username fails' do
     visit root_path
-    fill_in 'Name', with: 'test'
-    click_button 'Log In'
-    expect(page).to have_selector '.alert', text: "User doesn't exist. Try to sign up!"
+    fill_in 'user_name', with: 'test'
+    click_button 'LOG IN'
+    expect(page).to have_selector '.notification', text: "User not found. Click on sign up to create a new user."
   end
 
   scenario 'signing up with existent username fails' do
     test_user.save
     visit root_path
-    fill_in 'Name', with: 'test'
-    click_button 'Sign Up'
-    expect(page).to have_selector '.alert', text: 'User exists. Try to log in!'
+    fill_in 'user_name', with: 'test'
+    click_button 'SIGN UP'
+    expect(page).to have_selector '.notification', text: 'Name has already been taken'
   end
 
   scenario 'succesfull log in leads to user page' do
     test_user.save
     visit root_path
-    fill_in 'Name', with: 'test'
-    click_button 'Log In'
+    fill_in 'user_name', with: 'test'
+    click_button 'LOG IN'
     expect(current_path).to eq(user_path(test_user))
   end
 
   scenario 'succesful sign up leads to user page' do
     visit root_path
-    fill_in 'Name', with: 'test'
-    click_button 'Sign Up'
-    expect(page).to have_selector 'p', text: 'test'
+    fill_in 'user_name', with: 'test'
+    click_button 'SIGN UP'
+    expect(page).to have_selector 'h1', text: 'ALL MY TRACKS'
   end
 
   scenario 'visiting homepage while logged in leads you to the user page' do
-    test_user.save
-    visit root_path
-    fill_in 'Name', with: 'test'
-    click_button 'Log In'
+    login
     visit root_path
     expect(current_path).to eq(user_path(test_user))
   end
