08e129506ae8b571ee0d1ee9b6b7d7e5e1a71aa6
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-25 18:16:26 -0400

    Adds tests to the previous screen link

--- spec/views/layouts/application.html.erb_spec.rb
+++ spec/views/layouts/application.html.erb_spec.rb
@@ -2,29 +2,75 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe "layouts/application.html.erb", type: :feature do
-  let(:test_user) { User.new(name: 'test') }
 
-  def login
-    test_user.save
-    visit root_path
-    fill_in 'Name', with: 'test'
-    click_button 'Log In'
+  context 'links' do
+    scenario "all my tracks" do
+      login
+      click_link class: 'fa-bars'
+      click_link 'ALL MY TRACKS'
+      expect(current_path).to eq(user_path(test_user))
+    end
+
+    scenario 'my individual tracks' do
+      login
+      click_link class: 'fa-bars'
+      click_link 'MY INDIVIDUAL TRACKS'
+      expect(page).to have_current_path(user_path(test_user, groupless: true))
+    end
+
+    scenario 'all groups' do
+      login
+      click_link class: 'fa-bars'
+      click_link 'ALL GROUPS'
+      expect(current_path).to eq(groups_path)
+    end
+
+    scenario 'other users' do
+      login
+      click_link class: 'fa-bars'
+      click_link 'OTHER USERS'
+      expect(current_path).to eq(users_path)
+    end
+  
+    scenario 'logout' do
+      login
+      click_link class: 'fa-bars'
+      click_link 'LOG OUT'
+      visit user_path(test_user)
+      expect(current_path).to eq(root_path)
+    end
   end
+  
+  context 'previous screen' do
+    scenario 'main page' do
+      login
+      click_link class: 'fa-bars'
+      click_link class: 'miniature-anchor'
+      expect(current_path).to eq(user_path(test_user))
+    end
 
-  scenario 'logged user can logout' do
-    test_user.save
-    visit root_path
-    fill_in 'Name', with: 'test'
-    click_button 'Log In'
-    click_link 'Log out'
-    visit new_transaction_path
-    expect(current_path).to eq(root_path)
-  end
+    scenario 'my individual tracks' do
+      login
+      visit user_path(test_user, groupless: true)
+      click_link class: 'fa-bars'
+      click_link class: 'miniature-anchor'
+      expect(page).to have_current_path(user_path(test_user, groupless: true))
+    end
 
-  scenario "there is a link to check all user's transactions" do
-    login
-    visit new_transaction_path
-    click_link 'All my Transactions'
-    expect(current_path).to eq(user_path(test_user))
+    scenario 'all groups' do
+      login
+      visit groups_path
+      click_link class: 'fa-bars'
+      click_link class: 'miniature-anchor'
+      expect(current_path).to eq(groups_path)
+    end
+
+    scenario 'other users' do
+      login
+      visit users_path
+      click_link class: 'fa-bars'
+      click_link class: 'miniature-anchor'
+      expect(current_path).to eq(users_path)
+    end
   end
 end
