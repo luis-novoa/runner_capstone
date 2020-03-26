3f8d5d990bc33db22801edd010dc9fc654c0095f
Luis Novoa <luis.matte.novoa@gmail.com>
2020-03-26 12:32:27 -0400

    Adds tests to groups index

--- spec/views/groups/index.html.erb_spec.rb
+++ spec/views/groups/index.html.erb_spec.rb
@@ -7,4 +7,26 @@ RSpec.describe "groups/index.html.erb", type: :feature do
     visit groups_path
     expect(current_path).to eq(root_path)
   end
+
+  context 'logged in' do
+    scenario 'link to menu' do
+      login
+      visit groups_path
+      expect(page).to have_link href: groups_path(show_menu: true)
+    end
+
+    scenario "groups are listed" do
+      login
+      test_group2.save
+      visit groups_path
+      expect(page).to have_link href: group_path(test_group2)
+    end
+
+    scenario 'link to create group' do
+      login
+      visit groups_path
+      click_link 'CREATE NEW GROUP'
+      expect(current_path).to eq(create_group_path)
+    end
+  end
 end
